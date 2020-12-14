368c7831ee87135bc09de0c2afe30fb9ea79c7ad
William <wiliamwijaya1985@gmail.com>
2020-12-14 21:11:08 +0700

    Add reply text message feature

--- public/index.php
+++ public/index.php
@@ -54,6 +54,30 @@ $app->post('/webhook', function (Request $request, Response $response) use ($cha
     }
 
     // kode aplikasi nanti disini
+    // Reply text message
+    $data = json_decode($body, true);
+    if (is_array($data['events'])) {
+        foreach ($data['events'] as $event) {
+            if ($event['type'] == 'message') {
+                if ($event['message']['type'] == 'text') {
+                    // send same message as reply to user
+                    $result = $bot->replyText($event['replyToken'], $event['message']['text']);
 
+
+                    // or we can use replyMessage() instead to send reply message
+                    // $textMessageBuilder = new TextMessageBuilder($event['message']['text']);
+                    // $result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+
+
+                    $response->getBody()->write(json_encode($result->getJSONDecodedBody()));
+                    return $response
+                        ->withHeader('Content-Type', 'application/json')
+                        ->withStatus($result->getHTTPStatus());
+                }
+            }
+        }
+    }
 });
+$bot->replyText($replyToken, 'ini pesan balasan');
+
 $app->run();
